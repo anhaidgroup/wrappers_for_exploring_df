fcac56de461eac7989cb33077ce846ed15edf3d8
RenjieTang <renjietang27@gmail.com>
2017-05-09 18:02:40 -0500

    Add files via upload

---
+++ pandastable/pandastable_wrapper.py
@@ -0,0 +1,36 @@
+try:
+	from tkinter import *
+except ImportError as e:
+	from Tkinter import *
+from pandastable import Table, TableModel
+import pandas
+
+
+class explore_data(Frame):
+	"""Basic GUI frame for the table
+ 		Returns:
+		tkinter frame
+	"""
+	def __init__(self, df):
+		self.parent = None
+		Frame.__init__(self)
+		self.main = self.master
+		self.main.geometry('600x400+200+100')
+		self.main.title('Explore Data')
+		f = Frame(self.main)
+		f.pack(fill=BOTH,expand=1)
+        #set the table in the GUI
+		self.table = pt = Table(f, dataframe=df,
+			                    showtoolbar=True, showstatusbar=True)
+		pt.show()
+		return
+
+
+
+#df = pandas.read_csv('./2.csv')
+#app = ExploreData(df)
+
+#launch the app
+#app.mainloop()
+
+
